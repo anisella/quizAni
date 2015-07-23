cf95b0e3df09d153d5455f36bda641ec87f80475
Anisella <jilderali@gmail.com>
2015-07-23 18:02:23 -0500

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -40,12 +40,15 @@ exports.Quiz = Quiz;
 */
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
- Quiz.count().success(function(count){
+ Quiz.count().then(function(count){
   if(count===0){
 	Quiz.create({ pregunta: 'Capital de Italia',
 				  respuesta: 'Roma'
+				  });
+	Quiz.create({ pregunta: 'Capital de Portugal',
+				  respuesta: 'Lisboa'
 				  })
-		.success(function(){console.log('Base de datos inicializada')});
+		.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
